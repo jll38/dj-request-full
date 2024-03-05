993fd4cc08be84911cc8838ab806452c5866f48a
Julian Lechner <jll38@njit.edu>
2024-03-05 17:59:51 -0500

    implement Material UI and develop navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
+import Navbar from "./components/shared/Navbar";
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import "./App.css";
 import {
   createBrowserRouter,
@@ -8,26 +10,46 @@ import {
   Link,
 } from "react-router-dom";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2f27ce',
+    },
+    secondary: {
+      main: "#DEDCFF",
+    },
+
+  },
+});
+
+
 function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: (
         <div>
-          <h1>Hello World</h1>
-          <Link to="about">About Us</Link>
+          <h1>TuneToss</h1>
+          <Link to="about">Home</Link>
         </div>
       ),
     },
     {
       path: "about",
-      element: <div>About</div>,
+      element: (
+        <div>
+          <h1>TuneToss</h1>
+          <Link to="about">About Us</Link>
+        </div>
+      ),
     },
   ]);
 
   return (
     <div className="App">
+      <Navbar/>
       <RouterProvider router={router} />
+      <div></div>
     </div>
   );
 }
