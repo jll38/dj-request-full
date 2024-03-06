a99e04a6f6d8b64612fc339930d2349c0efc7fa2
Julian Lechner <jll38@njit.edu>
2024-03-05 20:13:34 -0500

    implement mongodb container

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -6,25 +6,16 @@ export default function Home() {
   return (
     <main className="container">
       <section
-        style={{ display: "flex", flexDirection: "column", gap: "20px" }}
+        style={{ display: "flex", alignItems: "center",gap: "20px", height: "600px" }}
       >
-        <div className="hero-img-container">
-          <img
-            className="hero-img"
-            srcSet={`${"https://images.pexels.com/photos/1677710/pexels-photo-1677710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-            src={`${"https://images.pexels.com/photos/1677710/pexels-photo-1677710.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"}?w=164&h=164&fit=crop&auto=format`}
-            alt={""}
-            loading="lazy"
-          />
-        </div>
-
         <div>
           <Typography
             order={2}
             variant={"h2"}
             fontWeight={800}
             fontSize={52}
-            lineHeight={0.85}
+            lineHeight={0.9}
+            
           >
             Command the <br />
             Dance Floor{" "}
@@ -38,14 +29,15 @@ export default function Home() {
             fontWeight={500}
             fontSize={20}
             lineHeight={0.85}
-            sx={{ maxWidth: "40%" }}
+            sx={{ maxWidth: "55%", marginY: 1}}
             color="gray"
           >
-            Welcome to <strong>TuneToss</strong>, where your musical wishes
-            command the night. Connect with the DJ like never before, tossing
-            your favorite tunes into the mix.
+            Connect with the DJ like never before, tossing your favorite tunes
+            into the mix.
           </Typography>
-          <Button sx={{marginY: 1}} variant="contained" color="primary">Get Started</Button>
+          <Button sx={{ marginY: 1 }} variant="contained" color="primary">
+            Get Started
+          </Button>
         </div>
       </section>
     </main>
